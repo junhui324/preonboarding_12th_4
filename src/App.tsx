b5cd36be7f7401010e760bd38935994fb3c0bc37
junhui324 <junhuijang00@gmail.com>
2023-09-10 14:18:45 +0900

    feat: 초기 셋팅

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { Suspense } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { ROUTES } from './constants/constants';
+import Main from './pages/MainPage/Main';
+import NotFound from './pages/NotFoundPage/NotFound';
+import LoadingSpinner from './components/loadingSpinner/LoadingSpinner';
+
+function App() {
+	return (
+		<>
+			{' '}
+			<Suspense fallback={<LoadingSpinner />}>
+				<Routes>
+					<Route path={ROUTES.MAIN} element={<Main />} />
+					<Route path={ROUTES.NOTFOUND} element={<NotFound />} />
+				</Routes>
+			</Suspense>
+		</>
+	);
+}
+
+export default App;
