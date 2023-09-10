6205799f446361ba4381b94daf662e79b471f110
junhui324 <junhuijang00@gmail.com>
2023-09-10 19:06:28 +0900

    chore: mook data 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,11 @@ import { Routes, Route } from 'react-router-dom';
 import { ROUTES } from './constants/constants';
 import Main from './pages/MainPage/Main';
 import NotFound from './pages/NotFoundPage/NotFound';
-import LoadingSpinner from './components/loadingSpinner/LoadingSpinner';
+import LoadingSpinner from './components/LoadingSpinner/LoadingSpinner';
 
 function App() {
 	return (
 		<>
-			{' '}
 			<Suspense fallback={<LoadingSpinner />}>
 				<Routes>
 					<Route path={ROUTES.MAIN} element={<Main />} />
