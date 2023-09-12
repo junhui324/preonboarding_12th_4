c7f32fabd7073bd524d632543b582b5f5ee9d566
junhui324 <junhuijang00@gmail.com>
2023-09-12 23:22:15 +0900

    feat: 버튼에서 선택한 ID값과 동일한 ID값을 가진 데이터 구역만 하이라이트 처리 구현 - recharts 라이브러리의 Cell을 사용하여 구현

--- src/components/TimeSeriesChart/TimeSeriesChart.tsx
+++ src/components/TimeSeriesChart/TimeSeriesChart.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { getData } from '../../api/Api';
+import FilterButton from './FilterButton';
 import styles from './TimeSeriesChart.module.scss';
+import { COLOR } from '../../constants/constants';
 import {
 	ComposedChart,
-	Line,
 	Bar,
 	XAxis,
 	YAxis,
@@ -12,6 +13,7 @@ import {
 	CartesianGrid,
 	ResponsiveContainer,
 	Area,
+	Cell,
 } from 'recharts';
 
 function TimeSeriesChart() {
@@ -20,94 +22,72 @@ function TimeSeriesChart() {
 	const [selectedID, setSelectedID] = useState<string | null>(null);
 
 	useEffect(() => {
-		async function fetchData() {
-			try {
-				const response = await getData();
-				const newChartData = Object.keys(response.response).map(timestamp => ({
-					timestamp,
-					value_area: response.response[timestamp].value_area,
-					value_bar: response.response[timestamp].value_bar,
-					id: response.response[timestamp].id,
-				}));
-
-				newChartData.forEach(entry => {
-					entry.timestamp = new Date(entry.timestamp).toLocaleTimeString();
-				});
-
-				const uniqueIds = [...new Set(newChartData.map(data => data.id))];
-
-				setChartData(newChartData);
-				setUniqueIds(uniqueIds);
-			} catch (error) {
-				console.error('데이터를 불러오는 중 오류 발생:', error);
-			}
-		}
-
 		fetchData();
 	}, []);
 
-	const handleIDSelect = (id: string) => {
-		setSelectedID(id);
+	const fetchData = async () => {
+		try {
+			const response = await getData();
+			changeRechartsData(response);
+		} catch (error) {
+			console.error('데이터를 불러오는 중 오류 발생:', error);
+		}
 	};
 
-	const renderBars = () => {
-		return chartData.map((data, index) => (
-			<Bar
-				key={index}
-				dataKey="value_bar"
-				fill={selectedID && selectedID === data.id ? '#ff0000' : '#82ca9d'}
-				opacity={1}
-				name="Bar"
-				yAxisId="right"
-			/>
-		));
-	};
+	const changeRechartsData = (response: any) => {
+		const newChartData = Object.keys(response.response).map(timestamp => ({
+			timestamp,
+			value_area: response.response[timestamp].value_area,
+			value_bar: response.response[timestamp].value_bar,
+			id: response.response[timestamp].id,
+		}));
 
-	console.log(selectedID);
+		newChartData.forEach(entry => {
+			entry.timestamp = new Date(entry.timestamp).toLocaleTimeString();
+		});
+
+		const uniqueIds = [...new Set(newChartData.map(data => data.id))];
+
+		setChartData(newChartData);
+		setUniqueIds(uniqueIds);
+	};
 
 	return (
 		<div>
-			<div className={styles.filterButtons}>
-				{uniqueIds
-					? uniqueIds.map(id => (
-							<button
-								key={id}
-								onClick={() => handleIDSelect(id)}
-								className={selectedID === id ? styles.selectedButton : ''}
-							>
-								{id}
-							</button>
-					  ))
-					: ''}
-			</div>
+			<FilterButton uniqueIds={uniqueIds} selectedID={selectedID} setSelectedID={setSelectedID} />
 			<ResponsiveContainer width="100%" height={400}>
 				<ComposedChart data={chartData}>
 					<XAxis dataKey="timestamp" />
-					<CartesianGrid strokeDasharray="3 3" />
+					<CartesianGrid stroke={COLOR.WHITE_GRAY} />
 					<Legend
 						payload={[
-							{ value: 'Area', type: 'rect', color: '#8884d8' },
-							{ value: 'Bar', type: 'rect', color: '#82ca9d' },
-							{ value: 'Highlighted Bar', type: 'rect', color: '#ff0000' },
+							{ value: 'Area', type: 'rect', color: COLOR.AREA },
+							{ value: 'Bar', type: 'rect', color: COLOR.BAR },
+							{ value: 'Highlighted Bar', type: 'rect', color: COLOR.HIGHLIGHTED_BAR },
 						]}
 					/>
 					<Tooltip content={<CustomTooltip />} />
-					<Area type="monotone" dataKey="value_area" fill="#8884d8" name="Area" yAxisId="left" />
-					<YAxis yAxisId="left" orientation="left" stroke="#8884d8" domain={[0, 'auto']} />
-					{/* <Bar dataKey="value_bar" fill="#82ca9d" name="Bar" yAxisId="right" /> */}
-					{renderBars()}
-					<YAxis yAxisId="right" orientation="right" stroke="#82ca9d" />
+
+					<Bar dataKey="value_bar" fill={COLOR.BAR} name="Bar" yAxisId="right">
+						{chartData.map((entry, index) => (
+							<Cell fill={entry.id === selectedID ? COLOR.HIGHLIGHTED_BAR : COLOR.BAR} />
+						))}
+					</Bar>
+					<YAxis yAxisId="right" orientation="right" stroke={COLOR.BAR} />
+					<Area type="monotone" dataKey="value_area" fill={COLOR.AREA} name="Area" yAxisId="left" />
+					<YAxis yAxisId="left" orientation="left" stroke={COLOR.AREA} domain={[0, 200]} />
 				</ComposedChart>
 			</ResponsiveContainer>
 		</div>
 	);
 }
 
-function CustomTooltip({ active, payload }: any) {
+function CustomTooltip({ active, payload, label }: any) {
 	if (active && payload && payload.length) {
 		const data = payload[0].payload;
 		return (
 			<div className={styles.customTooltip}>
+				<p className={styles.label}>{`${label}`}</p>
 				<p className={styles.id}>{`ID: ${data.id}`}</p>
 				<p className={styles.area}>{`Area: ${data.value_area}`}</p>
 				<p className={styles.bar}>{`Bar: ${data.value_bar}`}</p>
--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -12,3 +12,10 @@ export const MAX_LIST_NUM = 7;
 export const CACHE_NAME = 'sick';
 
 export const CACHE_DELETE_TIME = 10000;
+
+export const COLOR = {
+	AREA: '#ff8989',
+	BAR: '#a7a4e0',
+	HIGHLIGHTED_BAR: '#3c188f',
+	WHITE_GRAY: '#f5f5f5',
+};
